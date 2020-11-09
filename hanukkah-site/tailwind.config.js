4cb6bd938c5b0b75dbc6ef892ab33564640d6757
Samuel Pilay Muñoz <yhau1989@gmail.com>
2020-11-09 02:01:23 -0500

    add path components to tailwindcss purge PRD

--- hanukkah-site/tailwind.config.js
+++ hanukkah-site/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: ["./pages/**/*.js"],
+  purge: ["./pages/**/*.js", "./components/**/*.js"],
   theme: {
     extend: {},
   },
