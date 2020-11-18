e9acea72dcc71039bb9ea3c0eaa8206a2022f33f
Samuel Pilay Muñoz <yhau1989@gmail.com>
2020-11-18 12:18:02 -0500

    load lang from next.config

--- hanukkah-site/pages/_app.js
+++ hanukkah-site/pages/_app.js
@@ -1,14 +1,10 @@
 import '../styles/site.css'
 import { AuthProvider } from '../utils/auth'
-import { useEffect } from "react"
 import Head from "next/head"
 
 
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    document.documentElement.lang = "es";
-  }, ["es"]);
   return (
     <>
       <Head>
