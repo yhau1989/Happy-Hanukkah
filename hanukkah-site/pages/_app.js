e73d4264830744ef497616bf53f1f4ff4fc3a7bf
Samuel Pilay Muñoz <yhau1989@gmail.com>
2020-11-18 11:24:57 -0500

    add Internationalization and localization

--- hanukkah-site/pages/_app.js
+++ hanukkah-site/pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/site.css'
 import { AuthProvider } from '../utils/auth'
+import { useEffect } from "react"
+
+
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    document.documentElement.lang = "es";
+  }, ["es"]);
   return <AuthProvider>
     <Component {...pageProps} />
   </AuthProvider>
