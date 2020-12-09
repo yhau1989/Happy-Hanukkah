065568fdc7525b190000151556e156ea39bd1191
Samuel Pilay Muñoz <yhau1989@gmail.com>
2020-12-09 15:06:36 -0500

    remove next/image

--- hanukkah-site/components/bannerIntro.js
+++ hanukkah-site/components/bannerIntro.js
@@ -1,24 +1,39 @@
 import Link from "next/link";
-import Image from 'next/image'
+//import Image from 'next/image'
 import useDeviceDetect from "../utils/useDeviceDetect";
 
 export default function BannerIntro(params) {
     const {lenguage} = params
     const { isModerWeb } = useDeviceDetect();
 
+
     const bannerImgEn = () => {
-      let img = (isModerWeb)
+      let imgUrl = (isModerWeb)
       ? 'https://res.cloudinary.com/software-monkey-ecuador/image/upload/q_auto:best/v1606799084/happy-hanukkah/maxresdefault_wboyia.webp'
       : 'https://res.cloudinary.com/software-monkey-ecuador/image/upload/v1606799084/happy-hanukkah/maxresdefault_wboyia.jpg'
-      return <Image src={img} alt="What is Hanukkah?" height={700} width={1200} />
+      return imgUrl
     }
     
     const bannerImgEs = () => {      
-      let img2 = (isModerWeb)
+      let imgUrl2 = (isModerWeb)
       ? 'https://res.cloudinary.com/software-monkey-ecuador/image/upload/q_auto:best/v1606213496/happy-hanukkah/maxresdefault_ogbxpu.webp'
       : 'https://res.cloudinary.com/software-monkey-ecuador/image/upload/v1606213496/happy-hanukkah/maxresdefault_ogbxpu.jpg'
-      return <Image src={img2} alt="Que es Januca" height={700} width={1200} />
+      return imgUrl2
     } 
+
+    // const bannerImgEn = () => {
+    //   let img = (isModerWeb)
+    //   ? 'https://res.cloudinary.com/software-monkey-ecuador/image/upload/q_auto:best/v1606799084/happy-hanukkah/maxresdefault_wboyia.webp'
+    //   : 'https://res.cloudinary.com/software-monkey-ecuador/image/upload/v1606799084/happy-hanukkah/maxresdefault_wboyia.jpg'
+    //   return <Image src={img} alt="What is Hanukkah?" height={700} width={1200} />
+    // }
+    
+    // const bannerImgEs = () => {      
+    //   let img2 = (isModerWeb)
+    //   ? 'https://res.cloudinary.com/software-monkey-ecuador/image/upload/q_auto:best/v1606213496/happy-hanukkah/maxresdefault_ogbxpu.webp'
+    //   : 'https://res.cloudinary.com/software-monkey-ecuador/image/upload/v1606213496/happy-hanukkah/maxresdefault_ogbxpu.jpg'
+    //   return <Image src={img2} alt="Que es Januca" height={700} width={1200} />
+    // } 
 
     if(lenguage == "en")
     {
@@ -30,7 +45,14 @@ export default function BannerIntro(params) {
             rel="noopener noreferrer"
             aria-label="What is Hanukkah?"
           >
-            {bannerImgEn()}
+            {/* {bannerImgEn()} */}
+
+            <img
+              src={bannerImgEn()}
+              alt="What is Hanukkah?"
+              loading="lazy"
+            />
+
             <div className="absolute inset-14 sm:rounded-3xl flex flex-col justify-center">
               <svg
                 className="relative mx-auto w-16 h-16 md:w-32 md:h-32"
@@ -64,7 +86,12 @@ export default function BannerIntro(params) {
             rel="noopener noreferrer"
             aria-label="Que es Januca?"
           >
-            {bannerImgEs()}
+            {/* {bannerImgEs()} */}
+            <img
+              src={bannerImgEs()}
+              alt="Que es Januca?"
+              loading="lazy"
+            />
             <div className="absolute inset-14 sm:rounded-3xl flex flex-col justify-center">
               <svg
                 className="relative mx-auto w-16 h-16 md:w-32 md:h-32"
